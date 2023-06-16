696e3b0cbdf8725a267fc38009fa69dc63a3e47c
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-16 21:51:13 +0300

    Repository'nin Yazılması

---
+++ EasyCashIdentityProject.DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
