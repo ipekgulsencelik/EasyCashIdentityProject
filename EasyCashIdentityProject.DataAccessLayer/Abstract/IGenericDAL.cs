da81aad02b29b8c32b35bf94abfa55ebac805a33
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-16 21:50:43 +0300

    Interface'lerin Yazılması

---
+++ EasyCashIdentityProject.DataAccessLayer/Abstract/IGenericDAL.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyCashIdentityProject.DataAccessLayer.Abstract
+{
+    public interface IGenericDAL<T> where T : class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetByID(int id);
+        List<T> GetList();
+    }
+}
