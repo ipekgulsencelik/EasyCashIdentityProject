e898ab287d3630def84487d30565db0e881d9021
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-19 13:53:27 +0300

    Giriş Yapan Müşterinin Profil Bilgilerini Güncelleme

--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -1,11 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyCashIdentityProject.DTOLayer.DTOs.AppUserDTOs;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
+    [Authorize]
     public class MyAccountsController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        
+        public MyAccountsController(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            AppUserEditDTO appUserEditDTO = new AppUserEditDTO();
+            appUserEditDTO.Name = values.Name;
+            appUserEditDTO.Surname = values.Surname;
+            appUserEditDTO.PhoneNumber = values.PhoneNumber;
+            appUserEditDTO.Email = values.Email;
+            appUserEditDTO.City = values.City;
+            appUserEditDTO.District = values.District;
+            appUserEditDTO.ImageURL = values.ImageURL;
+
+            return View(appUserEditDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserEditDTO appUserEditDTO)
+        {
+            if (appUserEditDTO.Password == appUserEditDTO.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.PhoneNumber = appUserEditDTO.PhoneNumber;
+                user.Surname = appUserEditDTO.Surname;
+                user.City = appUserEditDTO.City;
+                user.District = appUserEditDTO.District;
+                user.Name = appUserEditDTO.Name;
+                user.ImageURL = "test";
+                user.Email = appUserEditDTO.Email;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, appUserEditDTO.Password);
+            
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+
             return View();
         }
     }
