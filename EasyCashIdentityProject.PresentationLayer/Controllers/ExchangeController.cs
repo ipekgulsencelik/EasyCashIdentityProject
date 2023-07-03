465421036bf7afa30edf3a8de00595805345c680
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-07-03 14:04:55 +0300

    Döviz Sayfası Düzenlenme

--- EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
@@ -66,6 +66,26 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             }
             #endregion
 
+            #region
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=EUR&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "6b5d9ea256msh2120b0d66e6f078p16642ejsn95a51d4014f3" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ViewBag.UsdToEur = body;
+            }
+            #endregion
+
             return View();
         }
     }
